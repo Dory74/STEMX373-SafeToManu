8d866c2685915b5a7ec048f49b0ed6f333eb4143
Jack <jackunsworth81@gmail.com>
2025-11-26 13:41:07 +1300

    Fixed pathing

--- manu.py
+++ manu.py
@@ -1,4 +1,7 @@
-import os
+import sys, os
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(PROJECT_ROOT, "samfiles"))
 # if using Apple MPS, fall back to CPU for unsupported ops
 os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
 import numpy as np
@@ -81,7 +84,7 @@ from sam2.build_sam import build_sam2
 from sam2.sam2_image_predictor import SAM2ImagePredictor
 
 sam2_checkpoint = "samfiles/checkpoints/sam2.1_hiera_large.pt"
-model_cfg = "samfiles/configs/sam2.1/sam2.1_hiera_l.yaml"
+model_cfg = "configs/sam2.1/sam2.1_hiera_l.yaml"
 
 sam2_model = build_sam2(model_cfg, sam2_checkpoint, device=device)
 
